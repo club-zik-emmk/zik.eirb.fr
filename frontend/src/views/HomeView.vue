47e4dd35c7a0452be84746907461a84fc2ae2133
Matteo Caravati <matteo.caravati@protonmail.com>
2022-11-24 17:58:50 +0100

    (feat) Added home background pictures

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="h-[92vh] overflow-hidden w-full">
     <!-- Photo showcase -->
-    <div class="wrapper relative w-full h-full">
+    <div class="wrapper relative w-full h-full" :style="backgroundStyle">
       <div class="h-full w-full absolute top-o left-0 bg-black opacity-20"></div>
       <div class="w-full h-full absolute top-0 left-0 z-10 flex items-center justify-center">
 
@@ -23,15 +23,39 @@
 
 <script>
 export default {
-  name: "HomeView"
+  name: "HomeView",
+  computed: {
+    backgroundStyle() {
+      let directory = '';
+      let randomNumber = -1;
+
+      if (this.isMobile) {
+        directory = 'mobile';
+
+        // Generate a random number between 00 and 01 (included)
+        randomNumber = Math.floor(Math.random() * 2);
+      } else {
+        directory = 'desktop';
+
+        // Generate a random number between 00 and 03 (included)
+        randomNumber = Math.floor(Math.random() * 4);
+      }
+
+      return {
+        '--url': `url('/img/${directory}/cover_${randomNumber.toString().padStart(2, '0')}.png')`
+      };
+    },
+    isMobile() {
+      return window.innerWidth < 768;
+    },
+  },
 }
 </script>
 
 <style>
 .wrapper {
-  background-image: url("@/assets/images/cover_00.png");
+  background-image: var(--url);
   background-size: cover;
   background-repeat: no-repeat;
 }
-
 </style>
