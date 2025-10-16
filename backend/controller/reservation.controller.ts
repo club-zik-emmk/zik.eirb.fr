c8d7441a42497a124b1a9e36e0ab10770a35b0cb
Siaz06 <110169015+Siaz06@users.noreply.github.com>
2025-10-16 22:28:43 +0200

    temp

--- backend/controller/reservation.controller.ts
+++ backend/controller/reservation.controller.ts
@@ -109,11 +109,16 @@ async function createOrUpdateReservation(req: Request, res: Response) {
     }
 
     try {
+        const ownerId =
+            (req.session && req.session.user && req.session.user.id)
+            || req.body.ownerId
+            || "ANONYMOUS";
+
         const insertedReservation = await Reservation.create({
             title: req.body.title,
             startDate: req.body.startDate,
             endDate: req.body.endDate,
-            ownerId: req.session.user.id,
+            ownerId: ownerId,
             allowNoise: typeof req.body.allowNoise !== "undefined" ? req.body.allowNoise : true,
         });
 
